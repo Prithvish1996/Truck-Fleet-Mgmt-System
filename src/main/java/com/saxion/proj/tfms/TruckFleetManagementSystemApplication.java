34b8cbf94c96fc085f0c876f6a380798edc75eae
PrithvishOK <582845@student.saxion.nl>
2025-09-12 15:32:03 +0200

    Initial Project skeleton structure
    Tech Stack:
    - Spring Boot
    - Spring Boot Microservices Architecture
    
    Notes:
    - Design may evolve based on requirements
    
    - Deployment strategy: local system and Kubernetes (k8s)
    
    - Kubernetes-related configuration to be added soon

---
+++ src/main/java/com/saxion/proj/tfms/TruckFleetManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.saxion.proj.tfms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckFleetManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TruckFleetManagementSystemApplication.class, args);
+    }
+
+}
