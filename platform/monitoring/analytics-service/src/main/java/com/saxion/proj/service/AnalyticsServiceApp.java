f974e7e1681fdd3d3db3aa6175f2ee2b5d22eae1
PrithvishOK <582845@student.saxion.nl>
2025-09-14 20:12:36 +0200

    Restructure of project

---
+++ platform/monitoring/analytics-service/src/main/java/com/saxion/proj/service/AnalyticsServiceApp.java
@@ -0,0 +1,15 @@
+package com.saxion.proj.service;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticsServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnalyticsServiceApp.class, args);
+      
+    }
+
+}
