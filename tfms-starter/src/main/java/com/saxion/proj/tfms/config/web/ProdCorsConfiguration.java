b3e52539ad6ab857a486bc1128ead67b9104980f
Adityanand Singh <586055@studend.saxion.nl>
2025-12-06 11:51:21 +0100

    Added Drivers + Planner in backend

--- tfms-starter/src/main/java/com/saxion/proj/tfms/config/web/ProdCorsConfiguration.java
+++ tfms-starter/src/main/java/com/saxion/proj/tfms/config/web/ProdCorsConfiguration.java
@@ -23,7 +23,7 @@ public class ProdCorsConfiguration {
         
         // Production domains only
         configuration.setAllowedOriginPatterns(Arrays.asList(
-            "https://yourdomain.com",
+            "http://localhost:3000",
             "https://www.yourdomain.com",
             "https://api.yourdomain.com"
         ));
